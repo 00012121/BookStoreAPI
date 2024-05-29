8b7b4b8f7a4686837c26e83c147b0e7fda316f8b
00012121 <wiut00012121@gmail.com>
2024-05-29 09:47:20 +0500

    Add project files.

---
+++ BookStoreAPI/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BookStoreAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string PublishYear { get; set; }
+
+        [JsonIgnore]
+        public Author Author { get; set; }
+
+        public int AuthorId { get; set; }
+    }
+}
