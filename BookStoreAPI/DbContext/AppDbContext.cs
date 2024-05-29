dd34fceb77ee57618d9d820b429c8a0e52012f29
00012121 <wiut00012121@gmail.com>
2024-05-29 10:14:24 +0500

    Migrations added

---
+++ BookStoreAPI/DbContext/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BookStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Author> Author { get; set; }
+
+    public DbSet<Book> Book { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(a => a.Id);
+            entity.Property(entity => entity.FirstName).IsRequired();
+
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasOne(a => a.Author).WithMany(p => p.Books).HasForeignKey(a => a.AuthorId).OnDelete(DeleteBehavior.NoAction);
+        });
+
+    }
+}
